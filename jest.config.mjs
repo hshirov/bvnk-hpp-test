2bcfde579ca7b33020607c7bcb19a75f33e32c49
Hristo Shirov <hshirov@abv.bg>
2025-04-18 21:24:11 +0300

    Fix issue with jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -100,7 +100,7 @@ const config = {
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: ['spec.ts'],
 
   // Activates notifications for test results
   // notify: false,
